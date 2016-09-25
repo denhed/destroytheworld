c64d0b182a442e5e3b6e1c3cb139412dd398ba77
Dennis Hedlund <denhed@Denniss-iMac.local>
2016-09-25 18:11:47 +0200

    Initial Commit

---
+++ Destroytheworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Destroytheworld
+//
+//  Created by Dennis Hedlund on 2016-09-25.
+//  Copyright © 2016 Dennis Hedlund. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
